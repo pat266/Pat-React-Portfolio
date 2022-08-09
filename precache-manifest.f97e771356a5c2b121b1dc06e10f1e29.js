ac622eb2159e4938fde08f9b92ef483669c3e82a
pat266 <69089199+pat266@users.noreply.github.com>
2022-08-09 19:23:39 -0400

    Updates

---
+++ precache-manifest.f97e771356a5c2b121b1dc06e10f1e29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01f8686f37e3618873b4",
+    "url": "/static/css/main.41c35e46.chunk.css"
+  },
+  {
+    "revision": "01f8686f37e3618873b4",
+    "url": "/static/js/main.01f8686f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "696e4a442ebdbab4e032b5353f417d31",
+    "url": "/index.html"
+  }
+];
