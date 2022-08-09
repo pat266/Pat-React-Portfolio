7f36df42b20c196129e19d822a10a0f1a7e67d87
pat266 <69089199+pat266@users.noreply.github.com>
2022-08-09 19:19:37 -0400

    Updates

---
+++ precache-manifest.0f8fa5993937565f6ad9a44b073ca272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb30f4a4445a388c1c11",
+    "url": "/Pat-React-Portfolio/static/css/main.41c35e46.chunk.css"
+  },
+  {
+    "revision": "cb30f4a4445a388c1c11",
+    "url": "/Pat-React-Portfolio/static/js/main.cb30f4a4.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Pat-React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "a1bbdcafb3afadb23002",
+    "url": "/Pat-React-Portfolio/static/js/runtime~main.a1bbdcaf.js"
+  },
+  {
+    "revision": "aeedbe964babd59e21b1d4f5d5ff1c86",
+    "url": "/Pat-React-Portfolio/index.html"
+  }
+];
