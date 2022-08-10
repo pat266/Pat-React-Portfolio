f30b2c9569a8a013bc37177e63bd5839c7f9d82e
pat266 <69089199+pat266@users.noreply.github.com>
2022-08-09 20:41:28 -0400

    Updates

---
+++ precache-manifest.86c0c9fca9b4d15d3cf851b63bea8f29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74e313cb5f0632edd8f7",
+    "url": "/pat266.github.io/static/css/main.41c35e46.chunk.css"
+  },
+  {
+    "revision": "74e313cb5f0632edd8f7",
+    "url": "/pat266.github.io/static/js/main.74e313cb.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/pat266.github.io/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e913c4942c1550f1217b",
+    "url": "/pat266.github.io/static/js/runtime~main.e913c494.js"
+  },
+  {
+    "revision": "5f95afb07588aaec55bf09ed5b6c3250",
+    "url": "/pat266.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Pat-React-Portfolio/precache-manifest.0f8fa5993937565f6ad9a44b073ca272.js"
+  "/pat266.github.io/precache-manifest.86c0c9fca9b4d15d3cf851b63bea8f29.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Pat-React-Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/pat266.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
